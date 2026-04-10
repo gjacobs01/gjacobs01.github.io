e7bfe4ea54caabd5a2c9636bece139736c3257d1
gjacobs <gavinrjacobs@gmail.com>
2026-04-10 17:33:52 -0400

    Nav bar linked to each page. Looking very nice.

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,3 +1,23 @@
+document.getElementById('navbar').innerHTML =`
+    <div class="navigation-bar">
+            <div class = "navigation-bar-title">
+                Gavin Jacobs  |  ENC 2135
+            </div>
+            <div class = "dropdown">
+                <button onclick = "showDropdown()" class = "dropdown-button">${getPageName()}<>  </button>
+                <div id = "nav-dropdown" class = "dropdown-content">  
+                    <a href="../index.html"> Home </a>
+                    <a href="../subpages/reflection.html"> Reflection </a>
+                    <a href="../subpages/revisions.html"> Revisons </a>
+                    <a href="../subpages/artifacts.html"> Artifacts </a>
+                </div>
+            </div>
+        </div>
+`;
+function getPageName(){
+    return document.querySelector('meta[name="page-title"]').getAttribute('content')
+}
+
 function showDropdown(){
     document.getElementById('nav-dropdown').classList.toggle("show");
 }
