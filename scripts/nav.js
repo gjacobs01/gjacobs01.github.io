4c9df5f1da335f451e9dd9c65c2a984c3a03f21e
gjacobs <gavinrjacobs@gmail.com>
2026-04-10 16:12:57 -0400

    very proud of my dropdown bar :D

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,1 +1,11 @@
-function getElementByID()
+function showDropdown(){
+    document.getElementById('nav-dropdown').classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if(!event.target.matches('.dropdown-button')){
+        var dropDowns = document.getElementsByClassName('dropdown-content')
+        for(var i = 0; i < dropDowns.length; i++)
+            dropDowns[i].classList.remove("show");
+    }
+}
